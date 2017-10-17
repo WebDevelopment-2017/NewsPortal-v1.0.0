d7454684ecac46fbc9643ee95789c93fe436adef
Satya190597 <32733153+Satya190597@users.noreply.github.com>
2017-10-17 21:01:07 +0530

    AngularJS code to get user and follow them
    Bahiya please check this file & mention that is this is the wright way to to write angularJS Code

--- WebContent/javascript/SearchFriends.js
+++ WebContent/javascript/SearchFriends.js
@@ -1,19 +1,61 @@
-app.controller("mycontroller_SearchFriends",function($scope,$http){
-		var mytext=document.getElementById("mytext").value;
-		$http.post("SearchFriends",'{"mytext":"'+mytext+'","myid":"'+$scope.id+'"}').then(function(response)
+//------------------------------------------------- My controller for find & follow other user ---------------------------------------------
+app.controller("mycontroller_SearchFriends",function($scope,$http)
+{
+		//----------- JSON Data ------------
+		$scope.mypostdata={};
+		$scope.addFriend={};
+		//----------------------------------
+		//------------------------------------------ Function to get user ------------------------------------
+		$scope.getOtherUsers=function()
 		{
-			$scope.mydata=response.data.myrecords;
-			if(response.data.myrecords[0]==null)
+			$scope.mypostdata.id=$scope.id
+			if($scope.mypostdata.mytext!="")
 			{
-				document.getElementById("message1").style.display="block";
+				//------------------------------- Post data to control-package->SearchFriends.java -------------------------------
+				$http.post("SearchFriends",$scope.mypostdata).then(function(response)
+				{
+					$scope.mydata=response.data.myrecords;
+					//-------------------------- If the records are empty ----------------------
+					if(response.data.myrecords[0]==null)
+					{
+						document.getElementById("message1").style.display="block";
+					}
+					//-------------------------- If else hide the message  ---------------------
+					else
+					{
+						document.getElementById("message1").style.display="none";
+					}
+				},
+				function(error)
+				{
+					alert("Sorry Something Went Wrong Please Logout And Login ...");
+				});
 			}
-			else
-			{
-				document.getElementById("message1").style.display="none";
-			}
-		},
-		function(error)
+		}
+		//------------------------------------------------------------------------------
+		//----------------------------- Function to follow user ------------------------
+		$scope.add=function(fid,name)
 		{
-			
-		});
+			$scope.addFriend.userId1=$scope.id;
+			$scope.addFriend.userId2=fid;
+			//------------------------------- Post data to dob-package->AddFriends.java -------------------------------
+			$http.post("AddFriends",$scope.addFriend).then(function(response)
+			{
+				if(response.data.flag==1)
+				{
+					alert("You are now following "+name+" !");
+					$scope.getOtherUsers();
+				}
+				else
+				{
+					alert("Sorry Something Went Wrong !");
+				}
+			},
+			function(error)
+			{
+				alert("Sorry Something Went Wrong Please Logout And Login ...");
+			});
+		}
+		//--------------------------------------------------------------------------------------------------------------
 });
+//------------------------------------------------------------------ End -------------------------------------------------------
